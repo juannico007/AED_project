73ca9adc3273e4f89cedf2b83d1c6bdaad2a0a5a
juannico007 <58712333+juannico007@users.noreply.github.com>
2022-05-13 08:38:30 -0500

    Add files via upload

---
+++ AED_proyecto_PCA.R
@@ -0,0 +1,58 @@
+library("readxl")
+library(factoextra)
+library(tidyverse)
+library("corrplot")
+idh <- read_excel("Documents/idh.xlsx")
+idh <- idh %>% remove_rownames() %>% column_to_rownames(var = 'País')
+View(idh)
+
+#PCA
+Data = idh[1:7]
+head(Data[, 1:7])
+
+#Valores y vecotres propios
+S = cov(Data)
+S = cov2cor(S)
+cov_eigen <- eigen(S)
+vecs = cov_eigen$vectors
+vals = cov_eigen$values
+
+#Estandarizamos debido a la diferencia en las escalas y unidades de variables
+#Sino, toda la variabilidad recaeria sobre el PIB
+PCA <-princomp(scale(Data))
+summary(PCA)
+
+var <- get_pca_var(PCA)
+cont = var$contrib
+view(cont)
+
+#La variable con mayor peso en la primera componente principal es el IDH
+#Variable con mayor peso en la 2 y en la 3 es Tasa de mortalidad
+
+#Con 3 componentes principales explicamos un 81% de la variabilidad
+
+#Plots de correlación de las variables con cada componente principal junto con su contribución a las mismas
+fviz_pca_var(PCA, axes = c(1,2), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
+fviz_pca_var(PCA, axes = c(1,3), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
+fviz_pca_var(PCA, axes = c(2,3), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
+
+#DE LA PRIMERA GRAFICA
+#Sobre la primera componente podemos ver que el IDH, la tasa de natalidad y el índice de fecundidad tienen una magnitud
+#De correlación similar para la primera componente principal
+#Mientras que para la segunda componente principal la que mayor correlacion tiene es la tasa de mortalidad
+#Tambien podemos ver que la tasa de natalidad y el índice de fecundidad estan inversamente correlacionados con la 
+#Tasa de mortalidad
+#El IDH, l indice de finalización de primaria, el PIB per cápita y la esperanza de vida estan positivamente correlacionadas
+#DE LA SGUNDA GRAFICA
+#Sobre la tercera componente la variable con mayor correlación es la tasa de mortalidad
+#La tasa de natalidad y el índice de fecundidad están inversamente correlacionados con la esperanza de vida
+
+corrplot(var$contrib, is.corr=FALSE)
+
+fviz_pca_ind(PCA, axes = c(1,2), geom.ind = "point", col.ind = idh$Tipo, palette = c("#00AFBB", "#E7B800", "#FC4E07"), addEllipses = TRUE, legend.title = "Grupos")
+fviz_pca_ind(PCA, axes = c(1,3), geom.ind = "point", col.ind = idh$Tipo, palette = c("#00AFBB", "#E7B800", "#FC4E07"), addEllipses = TRUE, legend.title = "Groups")
+fviz_pca_ind(PCA, axes = c(2,3), geom.ind = "point", col.ind = idh$Tipo, palette = c("#00AFBB", "#E7B800", "#FC4E07"), addEllipses = TRUE, legend.title = "Groups") 
+
+fviz_pca_ind(PCA, axes = c(1,2), geom.ind = "point", col.ind = idh$Continente, palette = c("#00AFBB", "#E7B800", "#FC4E07","#FB4EE7", "#AC4ED7", "#FC8E0F" ), addEllipses = TRUE, legend.title = "Grupos")
+
+ind <- get_pca_ind(PCA)
---
+++ AED_proyecto_regresion_stepwise.R
@@ -0,0 +1,23 @@
+library(readxl)
+idh <- read_excel("Documents/idh.xlsx")
+View(idh)    
+
+library(tidyverse)
+library(caret)
+library(leaps)
+
+#Regresión backwards
+Data = idh[2:8]
+
+#modelo unicamente con el intercepto
+intercept = lm(Data$IDH ~ 1)
+
+#Modelo con todas las predictoras
+full = lm(Data$IDH ~ ., data=Data)
+
+#Regresion backward stepwise 
+back = step(full, direction='backward', scope=formula(full), trace=0)
+
+#Vemos como el modelo que mejor se adata y explica la relacion de los datos excluye a las variables
+#de indice de fecundidad y tasa de mortalidad. 
+#Note como en este modelo todas las variables resultan ser significativas
